edc24d30097a6d32840a3bdaf819af23a0c44c35
zidong.wang <wangzidongprince@163.com>
2016-10-26 01:52:08 +0800

    buffer move

--- app/src/js/class/WheelClass.js
+++ app/src/js/class/WheelClass.js
@@ -11,6 +11,8 @@ var WheelClass = HClass.extend({
 
     step:30,//单个item的高
     skew:75,//这个为纠偏量 是指默认情况下item偏离正常位置的值
+
+    stopMoveFlag:false,
     ctor:function(id,textList,current){
         this.init(id,textList,current);
     },
@@ -49,7 +51,9 @@ var WheelClass = HClass.extend({
         }
         this.currentText = textList[this.currentIndex];
     },
-    resetPos:function(datY,isUpdate){//isUpdate 指是否更新currentIndex
+    resetPos:function(datY,isUpdate,fromBufferMove){
+    //isUpdate 指是否更新currentIndex    fromBufferMove 是否来源于缓冲
+        //如果来源于缓冲的话 要看是否触发边界  如果触发边界 则 isUpdate=true
         var needDuration = 0;
         if(typeof datY == "undefined"){
             datY = 0;
@@ -57,32 +61,16 @@ var WheelClass = HClass.extend({
         }
         //重置位置
 
-        var pos = this._cacularNowPos(datY);
-        this._transLateTo(-pos,needDuration);
+        var _this = this;
+        this._cacularNowPos(datY,function(pos,isend){//是否触发了边界
+            _this._transLateTo(-pos,needDuration);
+            if(isUpdate||(fromBufferMove&&isend)){
+                var currentIndex = _this._cacularNowIndex(datY);
+                _this.currentIndex=currentIndex;
+                _this.resetPos();
+            }
+        });
 
-        if(isUpdate){
-            var currentIndex = this._cacularNowIndex(datY);
-            this.currentIndex=currentIndex;
-            this.resetPos();
-        }
-    },
-    _cacularNowPos:function(datY){
-        var step = this.step;
-        var currentIndex = this.currentIndex;
-        var pos = currentIndex*step-this.skew-datY;//需要移动的pos位置
-
-        //边界检测
-        var  textList =this.textList;
-        var topIndex = textList.length+2;
-        var topPos = topIndex*step-this.skew;
-        if(pos>topPos){
-            pos = topPos;
-        }
-        if(pos<-2*this.skew){
-            pos = -2*this.skew;
-        }
-
-        return pos;
     },
     _cacularNowIndex:function(datY){
         var  textList =this.textList;
@@ -96,6 +84,31 @@ var WheelClass = HClass.extend({
             currentIndex = textList.length-1;
         }
         return currentIndex;
+    },
+    _cacularNowPos:function(datY,onCacular){
+        var step = this.step;
+        var currentIndex = this.currentIndex;
+        var pos = currentIndex*step-this.skew-datY;//需要移动的pos位置
+
+        //边界检测
+        var  textList =this.textList;
+        var topIndex = textList.length+2;
+        var topPos = topIndex*step-this.skew;
+        var isEnd = false;
+        if(pos>topPos){
+            pos = topPos;
+            this.stopBufferMove();
+            isEnd = true;
+        }
+        if(pos<-2*this.skew){
+            pos = -2*this.skew;
+            this.stopBufferMove();
+            isEnd = true;
+        }
+
+        if(onCacular){
+            onCacular(pos,isEnd);
+        }
     },
 
     _transLateTo:function(y,duration){
@@ -118,16 +131,20 @@ var WheelClass = HClass.extend({
         var touchCurrent = {};
         var touchEnd = {};
         var _this = this;
+        var timeStart = 0;//时间刻度
+        var timeEnd = 0;//时间刻度
+        var speed = 0;//瞬时速度
         elDom.addEventListener("touchstart",function(e){
             var touch = e.touches[0];
             touchStart = {
                 x:touch.pageX,
                 y:touch.pageY
             }
-            console.log(touchStart);
+            timeStart = new Date().getTime();
         },false);
         elDom.addEventListener("touchmove",function(e){
             var touch = e.touches[0];
+            var touchPre = touchCurrent||touchStart;
             touchCurrent = {
                 x:touch.pageX,
                 y:touch.pageY
@@ -135,20 +152,56 @@ var WheelClass = HClass.extend({
 
             var datY = touchCurrent.y-touchStart.y;
             _this.resetPos(datY);
-
-            console.log(touchCurrent);
+            //计算瞬时速度
+            timeEnd = new Date().getTime();
+            speed = (touchCurrent.y-touchPre.y)/(timeEnd-timeStart);
+            timeStart = timeEnd;
         },false);
         elDom.addEventListener("touchend",function(e){
             var touch = e.changedTouches[0];
+
             touchEnd = {
                 x:touch.pageX,
                 y:touch.pageY
             }
             var datY = touchEnd.y-touchStart.y;
-            _this.resetPos(datY,true);
-            console.log("end");
-            console.log(touchEnd);
+            //_this.resetPos(datY,true);
+            //缓冲运动
+            _this.bufferMove(datY,speed);
+            touchCurrent=null;
+            touchStart = null;
+            touchEnd = null;
         },false);
+    },
+    stopBufferMove:function(){
+        this.stopMoveFlag = true;
+    },
+    bufferMove:function(datY,v){
+        //v=-1;
+        this.stopMoveFlag = false;
+        this._move(0,v,datY);
+    },
+    _move:function(dt,v,datY){
+        var g = 0.005;//加速度
+        var vend = Math.abs(v)-dt*g;
+        console.log("vend:"+vend);
+        if(vend<0){
+            vend=0;
+        }
+        var dis = (v*v-vend*vend)/2/g;
+        var nowDatY = datY+v/Math.abs(v)*dis;
+        this.resetPos(nowDatY,vend==0,true);
+        var _this = this;
+        if(vend!=0){
+            if(!this.stopMoveFlag){
+                setTimeout(function(){
+                    _this._move(dt+20,v,datY);
+                },20);
+            }else{
+                this.stopMoveFlag = false;
+            }
+        }
+
     }
 });
 
